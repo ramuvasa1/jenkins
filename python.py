ca92675a0deae598e404fe34e4116f4d941f8749
Ramu Vasa <ramuvasa9@gmail.com>
2023-03-07 15:58:34 +0530

    python file error sloved

--- python.py
+++ python.py
@@ -2,7 +2,7 @@ li=[1,2,3,4,5]
 
 squares=[i**2 for i in li]
 
-cube=[j**3 for i in li]
+cube=[j**3 for j in li]
 
 print("The Squares of the Numbers are .",squares)
 print("The cube of the Numbers are .",cube)
